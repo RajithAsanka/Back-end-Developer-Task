7bf74f7479fdbaa26f3c43f9297b344e115da5ce
Rajith2012204 <rajith@fortunaglobal.com>
2019-10-15 22:22:21 +0530

    project setup files

---
+++ dcbs/src/main/java/com/back/end/task/dcbs/DummyCoreBankApplication.java
@@ -0,0 +1,13 @@
+package com.back.end.task.dcbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DummyCoreBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DummyCoreBankApplication.class, args);
+	}
+
+}
